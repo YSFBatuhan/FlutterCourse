20a0c12ba447174c2a00be460bfc20b2271e6c59
YSFBatuhan <141058317+YSFBatuhan@users.noreply.github.com>
2024-06-25 21:14:05 +0300

    Add files via upload
    Basit soru sormalı cevaplamalı oyun -> Youtube Ders 10

---
+++ BilgiOyunu/lib/cons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Icon CorrectIcon = Icon(
+  Icons.check,
+  color: Colors.green,
+);
+const Icon WrongIcon = Icon(
+  Icons.close,
+  color: Colors.red,
+);
